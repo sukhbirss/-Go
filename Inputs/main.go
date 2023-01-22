27280d772cbe7460e575446febe53cb1a3e9a637
sukhbirss <s.sukhbirsohal@gmail.com>
2023-01-22 20:50:32 +0530

    while loop

--- Inputs/main.go
+++ Inputs/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello Developer")
+	reader := bufio.NewReader(os.Stdin)
+	var stop bool = false
+	for !stop {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "Exit" {
+			stop = true
+		}
+		println(stop)
+
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sukhbirss
+
+go 1.19
